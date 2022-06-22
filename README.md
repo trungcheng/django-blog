bfc78733444fa395743549d01b83c7f08db3ed48
trung.dinh <trung.dinh@ntq-solution.com.vn>
2022-06-22 15:30:34 +0700

    application, routes, templates

---
+++ README.md
@@ -0,0 +1,23 @@
+# Django Blog
+- Application & Routes
+- Templates
+- Admin Page
+- Database & Migrations
+- User Registration
+- Login & Logout System
+- User Profile & Picture
+- Update User Profile
+- Create, Update, Delete Posts
+- Pagination
+- Email & Password Reset
+
+## Installation Step:
+
+1. pip install virtualenv 
+2. cd project_folder && virtualenv venv
+3. ./venv/Scripts/activate
+4. pip install -r requirements.txt
+5. cd app && python manage.py makemigrations
+6. python manage.py createsuperuser
+7. python manage.py migrate
+8. python manage.py runserver
---
+++ app/blog/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Trung Dinh',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 20, 2022',
+    },
+    {
+        'author': 'Ha Vu',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 21, 2022',
+    },
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {
+        'title': 'About'
+    })
