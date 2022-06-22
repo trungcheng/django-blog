0f42be4c678c0263f5eceba4b51813b6ad37823b
trung.dinh <trung.dinh@ntq-solution.com.vn>
2022-06-22 17:22:40 +0700

    database & migrations

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 posts = [
     {
@@ -16,12 +17,14 @@ posts = [
     },
 ]
 
+
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
 
     return render(request, 'blog/home.html', context)
+
 
 def about(request):
     return render(request, 'blog/about.html', {
